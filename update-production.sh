9c6745e982ba1ccae96156385ae2a35aeb005ac6
Marcus Chiu <marcuschiu9@gmail.com>
2025-06-04 20:00:42 -0500

    initial commit

---
+++ update-production.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+ssh -i ~/.ssh/keys/aws-marcuschiu.pem ec2-user@www.marcuschiu.com << EOF
+  rm -rf personal-website-two/
+  mkdir personal-website-two
+EOF
+
+scp -i ~/.ssh/keys/aws-marcuschiu.pem -r ./public ec2-user@www.marcuschiu.com:~/personal-website-two
