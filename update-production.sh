8e13c5c44ba384132c204adadd4266fbd76d5c83
Marcus Chiu <marcuschiu9@gmail.com>
2025-06-08 20:37:02 -0700

    initial commit

--- update-production.sh
+++ update-production.sh
@@ -2,17 +2,26 @@
 
 hugo
 
-tar czf public.tar.gz public
-
 ssh -i ~/.ssh/keys/aws-marcuschiu.pem ec2-user@www.marcuschiu.com << EOF
   rm -rf personal-website-two/
   mkdir personal-website-two
 EOF
 
-scp -i ~/.ssh/keys/aws-marcuschiu.pem -r ./public.tar.gz ec2-user@www.marcuschiu.com:~/personal-website-two
+##################
+# 1. REGULAR WAY #
+##################
 
-ssh -i ~/.ssh/keys/aws-marcuschiu.pem ec2-user@www.marcuschiu.com << EOF
-  cd personal-website-two
-  tar -xvzf public.tar.gz
-  rm public.tar.gz
-EOF
+scp -i ~/.ssh/keys/aws-marcuschiu.pem -r ./public ec2-user@www.marcuschiu.com:~/personal-website-two
+
+
+#####################
+# 2. COMPRESSED WAY #
+#####################
+
+#tar czf public.tar.gz public
+#scp -i ~/.ssh/keys/aws-marcuschiu.pem -r ./public.tar.gz ec2-user@www.marcuschiu.com:~/personal-website-two
+#ssh -i ~/.ssh/keys/aws-marcuschiu.pem ec2-user@www.marcuschiu.com << EOF
+#  cd personal-website-two
+#  tar -xvzf public.tar.gz
+#  rm public.tar.gz
+#EOF
