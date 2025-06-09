8eb1b6cdb222acd9c6e2a51376f34bf22d259d89
Marcus Chiu <marcuschiu9@gmail.com>
2025-06-08 22:26:27 -0700

    initial commit

--- update-production.sh
+++ update-production.sh
@@ -2,22 +2,42 @@
 
 hugo
 
+
+##############
+# 0. GIT WAY #
+##############
+
+cd public
+git commit -am "initial commit"
+git push
+
 ssh -i ~/.ssh/keys/aws-marcuschiu.pem ec2-user@www.marcuschiu.com << EOF
-  rm -rf personal-website-two/
-  mkdir personal-website-two
+  cd personal-website-two/public/
+  git pull
 EOF
+
 
 ##################
 # 1. REGULAR WAY #
 ##################
 
-scp -i ~/.ssh/keys/aws-marcuschiu.pem -r ./public ec2-user@www.marcuschiu.com:~/personal-website-two
+#ssh -i ~/.ssh/keys/aws-marcuschiu.pem ec2-user@www.marcuschiu.com << EOF
+#  rm -rf personal-website-two/
+#  mkdir personal-website-two
+#EOF
+#
+#scp -i ~/.ssh/keys/aws-marcuschiu.pem -r ./public ec2-user@www.marcuschiu.com:~/personal-website-two
 
 
 #####################
 # 2. COMPRESSED WAY #
 #####################
 
+#ssh -i ~/.ssh/keys/aws-marcuschiu.pem ec2-user@www.marcuschiu.com << EOF
+#  rm -rf personal-website-two/
+#  mkdir personal-website-two
+#EOF
+#
 #tar czf public.tar.gz public
 #scp -i ~/.ssh/keys/aws-marcuschiu.pem -r ./public.tar.gz ec2-user@www.marcuschiu.com:~/personal-website-two
 #ssh -i ~/.ssh/keys/aws-marcuschiu.pem ec2-user@www.marcuschiu.com << EOF
