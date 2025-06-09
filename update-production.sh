a4df48a33c6d4d1c70b2a0a4d1859ac22f6e728f
Marcus Chiu <marcuschiu9@gmail.com>
2025-06-08 20:29:08 -0700

    initial commit

--- update-production.sh
+++ update-production.sh
@@ -2,9 +2,17 @@
 
 hugo
 
+tar czf public.tar.gz public
+
 ssh -i ~/.ssh/keys/aws-marcuschiu.pem ec2-user@www.marcuschiu.com << EOF
   rm -rf personal-website-two/
   mkdir personal-website-two
 EOF
 
-scp -i ~/.ssh/keys/aws-marcuschiu.pem -r ./public ec2-user@www.marcuschiu.com:~/personal-website-two
+scp -i ~/.ssh/keys/aws-marcuschiu.pem -r ./public.tar.gz ec2-user@www.marcuschiu.com:~/personal-website-two
+
+ssh -i ~/.ssh/keys/aws-marcuschiu.pem ec2-user@www.marcuschiu.com << EOF
+  cd personal-website-two
+  tar -xvzf public.tar.gz
+  rm public.tar.gz
+EOF
