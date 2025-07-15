bb284de60ec0208050efa2f3bbef0ecba86c320b
Marcus Chiu <marcuschiu9@gmail.com>
2025-07-14 23:08:11 -0500

    initial commit

--- update-production.sh
+++ update-production.sh
@@ -12,7 +12,7 @@ git add .
 git commit -am "initial commit"
 git push
 
-ssh -i ~/.ssh/keys/aws-marcuschiu.pem ec2-user@www.marcuschiu.com << EOF
+ssh aws << EOF
   cd personal-website-two/public/
   git pull
 EOF
